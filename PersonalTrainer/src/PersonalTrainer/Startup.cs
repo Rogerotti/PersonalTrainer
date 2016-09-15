a0fac0cf4d144808e502441d8bd557629c48c317
Rogerotti <Rogerottii@gmail.com>
2016-09-15 22:16:29 +0200

    0.01

--- PersonalTrainer/src/PersonalTrainer/Startup.cs
+++ PersonalTrainer/src/PersonalTrainer/Startup.cs
@@ -1,31 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Framework.DataBaseContext;
+using Framework.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PersonalTrainer.ViewNavigator;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
 
 namespace PersonalTrainer
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private readonly IHostingEnvironment hostingEnviroment;
+
+        public Startup(IHostingEnvironment hostingEnviroment)
         {
-            //services.AddRouting();
-            services.AddMvc();
-            
+            this.hostingEnviroment = hostingEnviroment;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=PersonalTrainer;Trusted_Connection=True;";
+            services.AddDbContext<UserContext>(options => options.UseSqlServer(connection, b => b.MigrationsAssembly("PersonalTrainer")));
+
+            services.AddScoped<IUserManagement, UserManagement>();
+
+            var mvcBuilder = services.AddMvc();
+            IList<ModuleInfo> modules = GetModules(hostingEnviroment);
+
+            foreach (var module in modules)
+                mvcBuilder.AddApplicationPart(module.Assembly);
+
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Add(new ModuleViewLocationExpander());
+            });
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
-
+            
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
 
@@ -37,6 +57,57 @@ namespace PersonalTrainer
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        /// <summary>
+        /// Pozyskuje moduły aplikacji
+        /// </summary>
+        /// <param name="hostingEnviroment"></param>
+        /// <returns></returns>
+        private List<ModuleInfo> GetModules(IHostingEnvironment hostingEnviroment)
+        {
+            List<ModuleInfo> modules = new List<ModuleInfo>();
+
+            var moduleRootFolder = new DirectoryInfo(Path.Combine(hostingEnviroment.ContentRootPath, "Modules"));
+            var moduleFolders = moduleRootFolder.GetDirectories();
+            foreach (var moduleFolder in moduleFolders)
+            {
+                var binFolder = new DirectoryInfo(Path.Combine(moduleFolder.FullName, "bin"));
+                if (!binFolder.Exists)
+                {
+                    continue;
+                }
+
+                foreach (var file in binFolder.GetFileSystemInfos("*.dll", SearchOption.AllDirectories))
+                {
+                    Assembly assembly = null;
+                    try
+                    {
+                        assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file.FullName);
+                    }
+                    catch (FileLoadException ex)
+                    {
+                        if (ex.Message == "Assembly with same name is already loaded")
+                        {
+                            // Get loaded assembly
+                            assembly = Assembly.Load(new AssemblyName(Path.GetFileNameWithoutExtension(file.Name)));
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+
+                    if (assembly.FullName.Contains(moduleFolder.Name))
+                        modules.Add(new ModuleInfo
+                        {
+                            Name = moduleFolder.Name,
+                            Assembly = assembly,
+                            Path = moduleFolder.FullName
+                        });
+                }
+            }
+            return modules;
         }
     }
 }
